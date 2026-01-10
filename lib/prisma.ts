faa371d30906f1de2b928d124c46ad06f5dff5a0
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2026-01-10 18:27:46 -0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,13 @@
-import { PrismaClient } from "../lib/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({ log: ["query", "error", "warn"] });
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
