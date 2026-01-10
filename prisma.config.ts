ffe621a36b762a72f1597b6eaf3db397eb546c3f
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2026-01-10 18:55:25 -0300

    Released V1.0

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./lib/generated/prisma/client";
+import { defineConfig, env } from "prisma/config";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: `tsx prisma/seed.ts`,
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
